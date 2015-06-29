7f482ebdd22cf040ec7be49710facdc4c16aea4c
Efrén Reyes <efren.reyes@globant.com>
2015-06-29 14:27:17 -0500

    Initial Commit

---
+++ ProfileWithScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProfileWithScroll
+//
+//  Created by Efrén Reyes on 6/29/15.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
